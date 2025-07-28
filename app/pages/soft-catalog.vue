39e3d3ddd4be3f9286fbf49a38af1c9844e17bf0
Dmitry Sergeev <jitten@Mac.lan>
2025-07-28 13:48:39 +0300

    recursive search in soft page

--- app/pages/soft-catalog.vue
+++ app/pages/soft-catalog.vue
@@ -1,6 +1,10 @@
 <script setup>
 import fileTree from '../../public/soft-tree.json'
-const items = ref(fileTree)
+
+const treeItems = ref(fileTree)
+let srcTreeItems = treeItems.value
+const searchValue = ref('')
+let foundItems = []
 
 let downloadFile = (url, filename) => {
     const link = document.createElement('a');
@@ -15,14 +19,38 @@ const handleNodeSelect = (node) => {
   if (node === undefined || !Object.hasOwn(node,'value')) {return}
   downloadFile(node.value, node.filename);
 };
+
+const handleSearchInputChange = (event) => {
+  let searchVal = searchValue.value.toLowerCase().trim();
+  if (searchVal.length <= 0) {return}
+  foundItems = [];
+  searchItems(searchVal, srcTreeItems);
+  treeItems.value = foundItems;
+};
+
+let searchItems = (searchVal, items) => {
+    items.forEach((i) => {
+    let itemVal = i.label.toLowerCase();
+    if (itemVal.indexOf(searchVal) >= 0){
+      foundItems.push(i);
+      return; //same as continue, but in callback function
+    }
+    if (Object.hasOwn(i, 'children')){
+      searchItems(searchVal, i.children);
+    }
+  });
+};
 </script>
 <template>
   <UContainer class="max-w-192">
     <UCard>
         <template #header>
-            Softpack for malyutki
+          <div class="flex gap-3 align-middle items-center">
+            <span>Softpack for malyutki</span>
+            <UInput v-model="searchValue" placeholder="Search..." @change="handleSearchInputChange"/>
+          </div>
         </template>
-        <UTree :items="items" @update:model-value="handleNodeSelect"></UTree>
+        <UTree :items="treeItems" @update:model-value="handleNodeSelect"></UTree>
     </UCard>
   </UContainer>
 </template>
